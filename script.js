81fc358d3c849aaa8f465fb3fe5374a3529d1a19
Juan Cruz <juancruzbreppe@gmail.com>
2020-10-09 09:49:31 -0300

    Version 1.0.0
    En esta version es el proyecto base que cree, a partir de aca tengo que mejorar la animacion, header, footer y mas info relacionada a una clave segura

---
+++ script.js
@@ -0,0 +1,51 @@
+var boton = document.getElementById("b");
+var texto = document.getElementById("t");
+
+
+function randomClave(){
+  document.getElementById("t").innerHTML = '';
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    var c = document.getElementById("numC");
+    valor = c.value;
+    function clave(){
+      var numeros = '1234567890'
+      var minu = 'abcdefghijklmnopqrstuvwxyz';
+      var mayu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var caract ='-+*¿?!@#$&/(){}';
+      var k ='';
+        do {
+          var x ='';
+          for(var i =0;i<valor;i++){
+
+            num = Math.floor (Math.random() * 10);
+            minus = minu.charAt(Math.floor(Math.random()*minu.length));
+            mayus = mayu.charAt(Math.floor(Math.random()*mayu.length));
+            car = caract.charAt(Math.floor(Math.random()*caract.length));
+
+            ran = [num,minus,mayus,car];
+            var y='';
+            var z=0;
+            
+            z=Math.floor(Math.random() * (3-0 + 1));
+            
+            y= ran[z];
+
+            
+            x=x+y;
+            
+        }
+      } while( !strongRegex.test(x) );
+
+        return x;
+    }
+    
+    texto.classList.remove("line-1","anim-typewriter");
+    void texto.offsetWidth;
+    void texto.offsetHeight;
+    texto.classList.add("line-1","anim-typewriter");
+    document.getElementById("t").innerHTML = clave();
+    
+}
+
+
+boton.onclick = function(){randomClave()};
